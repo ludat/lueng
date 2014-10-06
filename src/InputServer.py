80d3cea87103bbdf7d6719f38ed91be28d16e9cf
Lucas David Traverso <lucas6246@gmail.com>
2014-10-06 12:47:27 -0300

    Added a command list using decorators

--- src/InputServer.py
+++ src/InputServer.py
@@ -19,6 +19,28 @@ class InputServer(threading.Thread):
         self._killed.clear()
         self.Widget = Widget
         self.name = 'SocketInputServer'
+        self.commands = {}
+
+        # I think the best way to implement a command list is a dictionary of
+        # functions, but I don't want them to be in the class global scope
+        # and this is an excellent opportunity to use decorators
+        @self.decorate
+        def kill(self):
+            logger.warning("I should be dead :D")
+            self.kill()
+            return "I'm dead\n"
+
+        @self.decorate
+        def list(self):
+            resp = ""
+            logger.debug("inside list command")
+            for widget in self.Widget.widgetsList:
+                resp += widget.name + "\n"
+            return resp
+
+    def decorate(self, func):
+        self.commands[func.__name__] = func
+        return func
 
     def run(self):
         if os.path.exists("/tmp/SB"):
@@ -40,22 +62,26 @@ class InputServer(threading.Thread):
         os.remove("/tmp/SB")
 
     def handle(self, conn, address):
+        # This will be very close to the mpd protocol
         ret = True
         try:
             data = conn.recv(1024).decode('utf-8')
-            logger.info("Recived: %s", repr(data))
+            logger.info("Received: %s", repr(data))
         except:
             logger.warning("recv failed")
-            ret = True
-        if data == "kill":
-            ret = False
+
+        commands = data.splitlines()
+        response = ""
+        for command in commands:
+            c = command.split(" ", 1)
+            logger.debug("Function: %s", c[0])
+            response += self.commands[c[0]](self)
 
         try:
-            conn.sendall(bytes(data, encoding="utf-8"))
-            logger.info("Sent: %s", repr(data))
+            conn.sendall(bytes(response, encoding="utf-8"))
+            logger.info("Sent: %s", repr(response))
         except:
             logger.warning("send failed")
-            ret = True
         conn.close()
         return ret
 
