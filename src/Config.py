b95e804eee6606596cb2cb8216f45d00fac66d7d
Lucas David Traverso <lucas6246@gmail.com>
2014-10-12 00:58:38 -0300

    Own logging on config and fixed bug when merging config files

--- src/Config.py
+++ src/Config.py
@@ -2,27 +2,44 @@
 
 import os
 import logging
+from colors import colors
 from ast import literal_eval as meval
 
 import collections
 
+formatterString = (
+    '{RESET}'
+    '{RED}{BOLD}%(levelname)-7s{RESET}:'
+    '{GREEN}{BOLD}%(name)-10s{RESET}:'
+    '%(message)s{RESET}'.format(**colors))
+formatter = logging.Formatter(formatterString)
+
+# Add ConsoleHandler
+consoleLogHandler = logging.StreamHandler()
+consoleLogHandler.name = 'Console Logger'
+consoleLogHandler.level = logging.DEBUG
+consoleLogHandler.formatter = formatter
+
 logger = logging.getLogger('CONFIG')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(consoleLogHandler)
 
 
 def update(d, u):
     for k, v in d.items():
-        if isinstance(u[k], type(d[k])):
-            if isinstance(v, collections.Mapping):
-                d[k] = update(d.get(k, {}), u.get(k, {}))
+        if k in u:
+            if type(u[k]) == type(d[k]):
+                if isinstance(v, collections.Mapping):
+                    d[k] = update(d.get(k, {}), u.get(k, {}))
+                else:
+                    d[k] = u[k]
             else:
-                d[k] = u[k]
-        else:
-            logger.warning(
-                "Data type mismatch {} != {}".format(
-                    str(type(d[k])),
-                    str(type(u[k]))
+                logger.warning(
+                    "Data type mismatch {} != {}".format(
+                        str(type(d[k])),
+                        str(type(u[k]))
+                    )
                 )
-            )
     return d
 
 USER_CONFIG = {}
