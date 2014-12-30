8b8ad3f6e804e0e014a6c431b22a62a6f64ed56b
Lucas David Traverso <lucas6246@gmail.com>
2014-12-30 04:15:13 -0300

    Little NOT WORKING refactor

---
+++ SB.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd "$( dirname "$0" )" && pwd
+python3 src/Main.py &
+MAIN_PID=$!
+
+function clean_up {
+    kill $(ps -o pid= --ppid ${MAIN_PID})
+    exit 0
+}
+trap clean_up SIGTERM SIGKILL SIGINT
+
+wait
