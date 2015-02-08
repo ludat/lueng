1d1c66ca552ba86ee0fb45e4fb18365c6139409a
Lucas David Traverso <lucas6246@gmail.com>
2015-02-08 06:02:50 -0300

    [widget][datetime]Updated to make it stand alone

--- widgets/DateAndTime.py
+++ widgets/DateAndTime.py
@@ -6,67 +6,28 @@ import logging
 import time
 import datetime
 
-IS_SAFE = True
-NAME = 'datetime'
 logger = logging.getLogger('WIDGET')
 
 
-class mainThread (threading.Thread):
-    def __init__(self, codeName, mainQueue, inputQueue=None):
-        threading.Thread.__init__(self)
-        self.name = NAME
-        self.codeName = codeName
-        self.mainQueue = mainQueue
-        self.lastUpdate = "dsadwa"
-        self._killed = threading.Event()
-        self._killed.clear()
+codeName = 'eee'
+lastUpdate = "dsadwa"
 
-    def run(self):
-        while True:
-            now = datetime.datetime.now()
-            result = now.strftime("%Y-%m-%d %H:%M:%S")
-            if self.killed():
-                break
-            self.updateContent(result)
-            time.sleep(1)
-        return 0
+def main():
+    while True:
+        now = datetime.datetime.now()
+        result = now.strftime("%Y-%m-%d %H:%M:%S")
+        updateContent(result)
+        time.sleep(1)
+    return 0
 
-    def updateContent(self, string):
-        if string != self.lastUpdate:
-            self.mainQueue.put({
-                'name': self.name,
-                'codeName': self.codeName,
-                'content': string})
-            self.lastUpdate = string
+def updateContent(string):
+    global lastUpdate
+    if string != lastUpdate:
+        printContent(string)
+        lastUpdate = string
 
-    def killed(self):
-        return self._killed.is_set()
-
-    def kill(self):
-        self._killed.set()
+def printContent(string):
+    print("{}|{}".format(codeName,string))
 
 if __name__ == "__main__":
-    import queue
-
-    class TestInputThread(threading.Thread):
-        def __init__(self, inputQueue):
-            threading.Thread.__init__(self)
-            self.inputQueue = inputQueue
-
-        def run(self):
-            while True:
-                self.inputQueue.put(input())
-
-    inputQueue = queue.Queue()
-    mainQueue = queue.Queue()
-
-    inputThread = TestInputThread(inputQueue)
-
-    thread = mainThread(
-        mainQueue,
-        inputQueue=inputQueue)
-    thread.start()
-    inputThread.start()
-
-    while True:
-        print(mainQueue.get()['content'])
+    main()
