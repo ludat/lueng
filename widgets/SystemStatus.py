4d5a4d7fc636b678135d6fd7b58aae009c9e9700
Lucas David Traverso <lucas6246@gmail.com>
2015-03-01 08:27:07 -0300

    Refactored SystemStatus for new style

--- widgets/SystemStatus.py
+++ widgets/SystemStatus.py
@@ -1,100 +1,58 @@
 #!/bin/env python3
 
-import threading
-import logging
+import sys
 
 from time import sleep
 import re
 
-IS_SAFE = True
-NAME = 'ramStatus'
-logger = logging.getLogger('WIDGET')
 WIDTH = 80
+lastUpdate = "dsadwa"
+memRegex = re.compile(
+    (
+        '(.*?):\D*?(\d+)\D*'
+    ),
+    re.DOTALL)
 
 
-class mainThread (threading.Thread):
-    def __init__(self, codeName, mainQueue, inputQueue=None):
-        threading.Thread.__init__(self)
-        self.name = NAME
-        self.codeName = codeName
-        self.mainQueue = mainQueue
-        self.lastUpdate = "dsadwa"
-        self._killed = threading.Event()
-        self._killed.clear()
-
-    def run(self):
-        self.memRegex = re.compile(
-            (
-                '(.*?):\D*?(\d+)\D*'
-            ),
-            re.DOTALL)
+def main():
         while True:
             memFile = open("/proc/meminfo")
             memRaw = memFile.read()
-            memDict = self.parseMemFile(memRaw)
-            result = self.format(memDict)
-            if self.killed():
-                break
-            self.updateContent(result)
+            memDict = parseMemFile(memRaw)
+            result = format(memDict)
+            updateContent(result)
             sleep(1)
         return 0
 
-    def format(self, d):
-        for k in d:
-            d[k] = int(d[k])
+def format(d):
+    for k in d:
+        d[k] = int(d[k])
 
-        free = round(d['MemAvailable'] * WIDTH / d['MemTotal'])
-        used = round((d['MemTotal'] - d['MemAvailable'] ) * WIDTH / d['MemTotal'])
+    free = round(d['MemAvailable'] * WIDTH / d['MemTotal'])
+    used = round((d['MemTotal'] - d['MemAvailable'] ) * WIDTH / d['MemTotal'])
 
-        return (
-            "^i(icons/xbm/mem.xbm) "
-            "^fg(#CCCCCC)^r({}x1)^fg(#999999)^r({}x1)^fg()".format(used, free)
-        )
+    return (
+        "^i(icons/xbm/mem.xbm) "
+        "^fg(#CCCCCC)^r({}x1)^fg(#999999)^r({}x1)^fg()".format(used, free)
+    )
 
-    def parseMemFile(self, raw):
-        memArr = raw.splitlines()
-        memDict = {}
-        for field in memArr:
-            key, value = self.memRegex.search(field).groups()
-            memDict[key] = value
-        return memDict
+def parseMemFile(raw):
+    memArr = raw.splitlines()
+    memDict = {}
+    for field in memArr:
+        key, value = memRegex.search(field).groups()
+        memDict[key] = value
+    return memDict
 
-    def updateContent(self, string):
-        if string != self.lastUpdate:
-            self.mainQueue.put({
-                'name': self.name,
-                'codeName': self.codeName,
-                'content': string})
-            self.lastUpdate = string
+def updateContent(string):
+    global lastUpdate
+    if string != lastUpdate:
+        printContent(string)
+        lastUpdate = string
 
-    def killed(self):
-        return self._killed.is_set()
-
-    def kill(self):
-        self._killed.set()
+def printContent(string):
+    sys.stdout.write(string+"\n")
+    sys.stdout.flush()
 
 if __name__ == "__main__":
-    import queue
-
-    class TestInputThread(threading.Thread):
-        def __init__(self, inputQueue):
-            threading.Thread.__init__(self)
-            self.inputQueue = inputQueue
-
-        def run(self):
-            while True:
-                self.inputQueue.put(input())
-
-    inputQueue = queue.Queue()
-    mainQueue = queue.Queue()
-
-    inputThread = TestInputThread(inputQueue)
-
-    thread = mainThread(
-        mainQueue,
-        inputQueue=inputQueue)
-    thread.start()
-    inputThread.start()
-
-    while True:
-        print(mainQueue.get()['content'])
+    main()
