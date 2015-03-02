5735f49375c427100632c5c9d2c00bb28aeddf72
Lucas David Traverso <lucas6246@gmail.com>
2015-03-02 05:25:17 -0300

    Refactored mpdStatus to be standalone

--- widgets/mpdStatus.py
+++ widgets/mpdStatus.py
@@ -1,182 +1,139 @@
 #!/bin/env python3
 
-import threading
-import logging
-
 import socket
 import time
-
-IS_SAFE = True
-NAME = 'mpdStatus'
-logger = logging.getLogger('WIDGET')
+import sys
 
 
-class mainThread (threading.Thread):
-    def __init__(self, codeName, mainQueue, inputQueue=None):
-        threading.Thread.__init__(self)
-        self.name = NAME
-        self.codeName = codeName
-        self.mainQueue = mainQueue
-        self.lastUpdate = "dsadwa"
-        self._killed = threading.Event()
-        self._killed.clear()
+lastUpdate = "dsadwa"
 
-    def run(self):
-        while True:
-            status = self.getResultOfCommand("status")
+def main():
+    while True:
+        status = getResultOfCommand("status")
+        result = ""
+        if 'error' in status:
+            result = status['error']
+            updateContent(parse(result))
+            time.sleep(10)
             result = ""
-            if 'error' in status:
-                result = status['error']
-                self.updateContent(self.parse(result))
-                time.sleep(10)
-                result = ""
-            elif status['state'] == "stop":
-                result = ""
-            elif status['state'] == "pause":
-                currentSong = self.getResultOfCommand("currentsong")
-                if 'error' in currentSong:
-                    result = currentSong['error']
-                else:
-                    result = (
-                        "^i(icons/xbm/pause.xbm) " +
-                        "{}% - {} - {}".format(
-                            status['volume'],
-                            currentSong['Title'],
-                            currentSong['Artist']
-                        )
-                    )
-            elif status['state'] == "play":
-                currentSong = self.getResultOfCommand("currentsong")
-                if 'error' in currentSong:
-                    result = currentSong['error']
-                else:
-                    result = (
-                        "^i(icons/xbm/play.xbm) " +
-                        "{}% - {} - {}".format(
-                            status['volume'],
-                            currentSong['Title'],
-                            currentSong['Artist']
-                        )
-                    )
+        elif status['state'] == "stop":
+            result = ""
+        elif status['state'] == "pause":
+            currentSong = getResultOfCommand("currentsong")
+            if 'error' in currentSong:
+                result = currentSong['error']
             else:
-                result = "SHIT"
+                result = (
+                    "^i(icons/xbm/pause.xbm) " +
+                    "{}% - {} - {}".format(
+                        status['volume'],
+                        currentSong['Title'],
+                        currentSong['Artist']
+                    )
+                )
+        elif status['state'] == "play":
+            currentSong = getResultOfCommand("currentsong")
+            if 'error' in currentSong:
+                result = currentSong['error']
+            else:
+                result = (
+                    "^i(icons/xbm/play.xbm) " +
+                    "{}% - {} - {}".format(
+                        status['volume'],
+                        currentSong['Title'],
+                        currentSong['Artist']
+                    )
+                )
+        else:
+            result = "SHIT"
 
-            if self.killed():
-                break
-            self.updateContent(self.parse(result))
-            self.idle()
-        return 0
+        updateContent(parse(result))
+        idle()
+    return 0
 
-    def idle(self):
-        "Wait until mpd is on and something has happened"
-        while True:
-            data = None
-            try:
-                sock = self.connectTcp('localhost', 6600, timeout=None)
-                sock.sendall(bytes("idle\n", encoding="utf-8"))
-                data = ""
-                while True:
-                    data = sock.recv(4096).decode("utf-8")
-                    if data == "" or data.endswith("OK\n"):
-                        break
-                sock.close()
-            except:
-                time.sleep(2)
-            if data is not None:
-                return
-
-    def getResultOfCommand(self, command):
-        "Get the result of sending a command to the mpd server"
-        data = ""
+def idle():
+    "Wait until mpd is on and something has happened"
+    while True:
+        data = None
         try:
-            sock = self.connectTcp('localhost', 6600)
-            sock.sendall(bytes(command + "\n", encoding="utf-8"))
+            sock = connectTcp('localhost', 6600, timeout=None)
+            sock.sendall(bytes("idle\n", encoding="utf-8"))
+            data = ""
             while True:
-                data += sock.recv(4096).decode("utf-8")
+                data = sock.recv(4096).decode("utf-8")
                 if data == "" or data.endswith("OK\n"):
                     break
             sock.close()
         except:
-            return {'error': 'Connection error'}
+            time.sleep(2)
+        if data is not None:
+            return
 
-        return self.parseInput(data)
+def getResultOfCommand(command):
+    "Get the result of sending a command to the mpd server"
+    data = ""
+    try:
+        sock = connectTcp('localhost', 6600)
+        sock.sendall(bytes(command + "\n", encoding="utf-8"))
+        while True:
+            data += sock.recv(4096).decode("utf-8")
+            if data == "" or data.endswith("OK\n"):
+                break
+        sock.close()
+    except:
+        return {'error': 'Connection error'}
 
-    def parseInput(self, data):
-        result = {}
-        lines = data.splitlines()
-        for line in lines:
-            if ": " in line:
-                key, content = line.split(": ", 1)
-                result[key] = content
-        return result
+    return parseInput(data)
 
-    def connectTcp(self, host, port, timeout=2):
-        "Abstraction for tcp connections"
+def parseInput(data):
+    result = {}
+    lines = data.splitlines()
+    for line in lines:
+        if ": " in line:
+            key, content = line.split(": ", 1)
+            result[key] = content
+    return result
+
+def connectTcp(host, port, timeout=2):
+    "Abstraction for tcp connections"
+    try:
+        flags = socket.AI_ADDRCONFIG
+    except AttributeError:
+        flags = 0
+    err = None
+    for res in socket.getaddrinfo(host, port, socket.AF_UNSPEC,
+                                  socket.SOCK_STREAM, socket.IPPROTO_TCP,
+                                  flags):
+        af, socktype, proto, canonname, sa = res
+        sock = None
         try:
-            flags = socket.AI_ADDRCONFIG
-        except AttributeError:
-            flags = 0
-        err = None
-        for res in socket.getaddrinfo(host, port, socket.AF_UNSPEC,
-                                      socket.SOCK_STREAM, socket.IPPROTO_TCP,
-                                      flags):
-            af, socktype, proto, canonname, sa = res
-            sock = None
-            try:
-                sock = socket.socket(af, socktype, proto)
-                sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
-                sock.settimeout(timeout)
-                sock.connect(sa)
-                return sock
-            except socket.error as e:
-                err = e
-                if sock is not None:
-                    sock.close()
-        if err is not None:
-            raise err
-        else:
-            raise ConnectionError("getaddrinfo returns an empty list")
+            sock = socket.socket(af, socktype, proto)
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+            sock.settimeout(timeout)
+            sock.connect(sa)
+            return sock
+        except socket.error as e:
+            err = e
+            if sock is not None:
+                sock.close()
+    if err is not None:
+        raise err
+    else:
+        raise ConnectionError("getaddrinfo returns an empty list")
 
-    def updateContent(self, string):
-        if string != self.lastUpdate:
-            self.mainQueue.put({
-                'name': self.name,
-                'codeName': self.codeName,
-                'content': string})
-            self.lastUpdate = string
 
-    def parse(self, string):
-        return string
+def parse(string):
+    return string
 
-    def killed(self):
-        return self._killed.is_set()
+def updateContent(string):
+    global lastUpdate
+    if string != lastUpdate:
+        printContent(string)
+        lastUpdate = string
 
-    def kill(self):
-        self._killed.set()
-
+def printContent(string):
+    sys.stdout.write(string+"\n")
+    sys.stdout.flush()
 
 if __name__ == "__main__":
-    import queue
-
-    class TestInputThread(threading.Thread):
-        def __init__(self, inputQueue):
-            threading.Thread.__init__(self)
-            self.inputQueue = inputQueue
-
-        def run(self):
-            while True:
-                self.inputQueue.put(input())
-
-    inputQueue = queue.Queue()
-    mainQueue = queue.Queue()
-
-    inputThread = TestInputThread(inputQueue)
-
-    thread = mainThread(
-        mainQueue,
-        inputQueue=inputQueue)
-    thread.start()
-    inputThread.start()
-
-    while True:
-        print(mainQueue.get()['content'])
+    main()
