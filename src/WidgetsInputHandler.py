269aab32c6ee15007fc7ec10a06608eea540c88d
Lucas David Traverso <lucas6246@gmail.com>
2015-03-02 09:50:52 -0300

    Improved widgetInputHandler thread

--- src/WidgetsInputHandler.py
+++ src/WidgetsInputHandler.py
@@ -29,32 +29,32 @@ class WidgetsInputHandler (threading.Thread):
                 break
 
             with epoll() as poll:
-                logger.debug("Setting up polling")
                 poll.register(self.eventPipe)
                 for widget in self.Widget.List:
                     poll.register(widget.p.stdout)
 
-                logger.debug("Started polling")
                 p, t = poll.poll()[0]
-                logger.debug("Got poll")
                 codename = None
                 for widget in self.Widget.List:
                     if widget.p.stdout.fileno() == p:
                         codename = widget.codeName
                         break
                 else:
-                    logger.debug("widget id not found")
+                    logger.warn("widget id not found")
                     continue
 
-                if t == EPOLLIN:
+                if t & EPOLLIN:
                     for widget in self.Widget.List:
                         if widget.codeName == codename:
-                            widget.updateContent(widget.p.stdout.readline()[:-1])
-                            continue
+                            data = widget.p.stdout.readline().replace("\n","")
+                            widget.updateContent(data)
+                            break
                     output = self.Widget.parseToString()
                     logger.debug("Updated output:\n\t%s", output)
                     self.outputStream.write(output)
                     self.outputStream.flush()
+                elif t & EPOLLHUP:
+                    logger.warn("Got SIGHUP", t)
                 else:
                     logger.warn("Some weird code %d", t)
 
